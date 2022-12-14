bb92432f2ca566fdd68ad97c1e51991f6c00754d
harem123 <cp1085275@gmail.com>
2022-12-13 21:03:17 -0500

    db config

--- serve.js
+++ serve.js
@@ -2,13 +2,22 @@
 const express = require("express");
 const app = express();
 
+const db = require("./models/index.js");
+const userModel = db.user;
+
 require('dotenv').config();
 const PORT = process.env.PORT || 3000
+
 app.get('/', async (req, res) => {
     
     res.json(`Running at port:${PORT}`)
   })
 
+  app.get('/data', async (req, res) => {
+    const data = userModel.findAll()
+    res.json(data)
+  })
+  
 
 app.listen(PORT, () => {
   console.log(`Running at localhost:${PORT}`);

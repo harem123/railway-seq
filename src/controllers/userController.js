9b72f33229c1fde8982895490e0850e795c1789a
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2022-12-19 16:10:59 -0500

    update controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,2 +1,14 @@
 const db = require("../../models/index.js");
 const userModel = db.user;
+
+const listUsers = async(req,res) =>{  
+    try{
+      const data = await userModel.findAll()
+    console.log(data)
+      res.status(200).send(data);
+      }
+      catch (error) {
+        console.log(error)
+        res.status(500).send({status:"FAILED"});
+      } 
+}

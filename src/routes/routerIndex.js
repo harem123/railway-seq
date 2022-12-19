2041c897b20de3906408a2a959259b31073891f9
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2022-12-19 15:22:18 -0500

    update port

--- src/routes/routerIndex.js
+++ src/routes/routerIndex.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const router =  express.Router()
 
+require('dotenv').config();
+const PORT = process.env.PORT || 3000
+
 const db = require("./models/index.js");
 const userModel = db.user;
 

45484568874189b94cd83316721dfc6e5a7f0206
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2022-12-19 16:07:01 -0500

    refactor endpoints

--- src/routes/routerIndex.js
+++ src/routes/routerIndex.js
@@ -1,22 +1,19 @@
 const express = require("express")
 const router =  express.Router()
 
+const v1UserController = require('../controllers/userController.js')
+
 require('dotenv').config();
 const PORT = process.env.PORT || 3000
 
 const db = require("./models/index.js");
 const userModel = db.user;
 
-app.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
     res.json(`Running at port:${PORT}`)
   })
-
-  app.get('/data', async (req, res) => {
-    const data = await userModel.findAll()
-    console.log(data)
-    res.json(data)
-    
-  })
+router.get("/user", v1UserController.listUsers);
+ 
 
 // exporting modules
 module.exports = router

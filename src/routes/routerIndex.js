9070e4752d66d31dae454b2055d5383913d60247
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2022-12-19 15:20:07 -0500

    update router index

--- src/routes/routerIndex.js
+++ src/routes/routerIndex.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const router =  express.Router()
 
+const db = require("./models/index.js");
+const userModel = db.user;
+
+app.get('/', async (req, res) => {
+    res.json(`Running at port:${PORT}`)
+  })
+
+  app.get('/data', async (req, res) => {
+    const data = await userModel.findAll()
+    console.log(data)
+    res.json(data)
+    
+  })
 
 // exporting modules
 module.exports = router

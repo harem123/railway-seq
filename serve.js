dc994790d2bcb51551dac1b0ce849e59d3febaee
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2022-12-19 15:20:31 -0500

    update serve

--- serve.js
+++ serve.js
@@ -2,24 +2,11 @@
 const express = require("express");
 const app = express();
 
-const db = require("./models/index.js");
-const userModel = db.user;
+const v1Router = require('./src/routes/routerIndex.js')
+app.use("/", v1Router)
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3000
-
-app.get('/', async (req, res) => {
-    
-    res.json(`Running at port:${PORT}`)
-  })
-
-  app.get('/data', async (req, res) => {
-    const data = await userModel.findAll()
-    console.log(data)
-    res.json(data)
-    
-  })
-  
 
 app.listen(PORT, () => {
   console.log(`Running at localhost:${PORT}`);
